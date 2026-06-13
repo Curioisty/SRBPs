a4b59ee46e0a345c2951de394e741de280220dbc
haoqi zhu <hqzhu005@gmail.com>
2026-06-13 21:53:48 +0800

    Delete Short-chain diffusion simulation/KA3 directory

--- Short-chain diffusion simulation/KA3/step3_yibu/source/ana.sh
+++
@@ -1,3 +0,0 @@
-#awk '$1 == 1' out.xyz | awk 'NR % 2 == 1 {print > "x1.txt"; next} {print > "x2.txt"}'
-
-awk '/ITEM: ATOMS id x y z/{for(i=1;i<=3006;i++){getline; print}}'  jieguo2.lammpstrj  > all.txt
--- Short-chain diffusion simulation/KA3/step4_qiazhu/source/ana.sh
+++
@@ -1,5 +0,0 @@
-#awk '$1 == 1' out.xyz | awk 'NR % 2 == 1 {print > "x1.txt"; next} {print > "x2.txt"}'
-
-#awk '/ITEM: ATOMS id x y z/{for(i=1;i<=3006;i++){getline; print}}'  jieguo2.lammpstrj  > all.txt
-#
-awk '/^2 / {print $2, $3, $4}' out.xyz > a1.txt
